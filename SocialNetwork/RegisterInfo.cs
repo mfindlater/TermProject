a8993aa1c07c598cc4bbbe45f84cc785e01822ca
Chialing Chang <tug98770@temple.edu>
2018-11-19 12:34:38 -0500

    class: RegisterInfo, enum: FriendRequestStatus

---
+++ SocialNetwork/RegisterInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork
+{
+    public class RegisterInfo : UserInfo
+    {
+        public string Password { get; set; }
+        public List<SecurityQuestion> SecurityQuestions { get; set; }
+    }
+}
