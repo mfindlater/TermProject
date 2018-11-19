bd79d35095090bb7a576bf5cecee9e1497d310f0
Matthew Findlater <matthew.findlater@gmail.com>
2018-11-19 12:17:46 -0500

    friend class

---
+++ SocialNetwork/Friend.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork
+{
+    public class Friend
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string ProfilePhotoURL { get; set; }
+    }
+}
