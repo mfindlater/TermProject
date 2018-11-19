1118a65898eda6f3b8f45f4dce1a9fa8409479fe
Matthew Findlater <matthew.findlater@gmail.com>
2018-11-19 14:59:30 -0500

    master page

---
+++ TermProjectLogin/Common.Master.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TermProjectLogin
+{
+    public partial class Common : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /*
+            var user = Response.Cookies.Get("User");
+
+            bool isLoggedIn = Convert.ToBoolean(user.Values["IsLoggedIn"]);
+
+            if(isLoggedIn)
+            {
+                pnLogin.Visible = false;
+                pnLoggedIn.Visible = true;
+            }
+            else
+            {
+                pnLogin.Visible = true;
+                pnLoggedIn.Visible = false;
+            } 
+            */
+        }
+    }
+}
