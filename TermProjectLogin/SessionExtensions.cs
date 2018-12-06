a9099881805aec0bd92acdcad71955db639f4283
Matthew Findlater <matthew.findlater@gmail.com>
2018-12-05 19:05:31 -0500

    session extensions

---
+++ TermProjectLogin/SessionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using SocialNetwork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+
+namespace TermProjectLogin
+{
+    public static class SessionExtensions
+    {
+        public static User GetUser(this HttpSessionState session)
+        {
+            return (User)session[Constants.UserSession];
+        }
+    }
+}
