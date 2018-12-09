e58820d49149e6ebe25b8f991010a8a5063a0cf9
Chialing Chang <tug98770@temple.edu>
2018-12-09 17:34:16 -0500

    PhotoPage: Photos, Create Album, Albums

--- SocialNetwork/PhotoAlbum.cs
+++ SocialNetwork/PhotoAlbum.cs
@@ -12,6 +12,6 @@ namespace SocialNetwork
         public string Name { get; set; }
         public string Description { get; set; }
         public int UserID { get; set; }
-        public List<Photo> Photos { get; set; }
+        public List<Photo> Photos { get; set; } = new List<Photo>();
     }
 }
